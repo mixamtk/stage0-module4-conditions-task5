c6d9308104a9166fb86bc2b3aaa6f6f00be85916
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-06-30 17:10:27 +0300

    Update BasicSwitchBreaks.java

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitchBreaks.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitchBreaks.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task5;
 
 public class BasicSwitchBreaks {
     public static void main(String[] args) {
-        int a = 20;
+        int a = 0;
                 switch (a) {
             default:
                 System.out.println("default");
@@ -15,6 +15,7 @@ public class BasicSwitchBreaks {
                 break;
             case 20:
                 System.out.println("twenty");
+                break;
         } 
     }
 }
